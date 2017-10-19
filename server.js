dc4b5fddf1949edc75b1596ebb9b8a04c4360fdf
lidak <animatio.dev@gmail.com>
2017-10-19 16:11:36 +0300

    initial-commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io').listen(server);
+
+const users = [];
+const connections = [];
+
+server.listen(process.env.PORT || 3000);
+console.log('The server is up and running');
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', (socket) => {
+  connections.push(socket);
+
+  socket.on('disconnect', () => {
+    connections.splice(connections.indexOf(socket), 1);
+    console.log(`The amount of connections is ${connections.length}`)
+  });
+
+  socket.on('send message', (data) => {
+    io.sockets.emit('new message', { message: data });
+  });
+});
+
+
+
